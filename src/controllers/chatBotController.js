f5dc569c45cdab2b6a14daf4100a0c6f5d79d9b8
= <=>
2021-10-26 07:51:23 +0800

    update

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -153,13 +153,13 @@ function handleMessage(sender_psid, message) {
   let entityChosen = "";
 
   entitiesArr.forEach((name) => {
-    // let entity = firstTrait(message.nlp, name);
+    let entity = firstTrait(message.nlp, name);
     // if(entity && entity.confidence > 0.8) {
     //   callSendAPI(sender_psid, entity+', '+name);
     //   entityChosen = name;
     // }
 
-    callSendAPI(sender_psid, name);
+    callSendAPI(sender_psid, entity);
   });
 
   if(entityChosen === "") {
