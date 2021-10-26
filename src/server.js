8fe771b6c5a54bb7624106e368157ae1512e53b8
= <=>
2021-10-27 03:50:58 +0800

    update

--- src/server.js
+++ src/server.js
@@ -17,43 +17,43 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-//         //this code will get the intent triggered in dialogflow through json
-//         app.post("/webhook", function(request, response) {
-//         // let _agent = new WebhookClient({request,response});
-//         //const fulfillment = request.body.queryResult.fulfillmentText;
-//         const fulfillment = request.body.queryResult.fulfillmentMessages[0].text.text[0];
-//         const obj = {fulfillment};
-//         console.log("json string is" + JSON.stringify(obj));
-//         response.send(JSON.stringify(obj));
+        //this code will get the intent triggered in dialogflow through json
+        app.post("/webhook", function(request, response) {
+        // let _agent = new WebhookClient({request,response});
+        //const fulfillment = request.body.queryResult.fulfillmentText;
+        const fulfillment = request.body.queryResult.fulfillmentMessages[0].text.text[0];
+        const obj = {fulfillment};
+        console.log("json string is" + JSON.stringify(obj));
+        response.send(JSON.stringify(obj));
 
 // });
 
-app.post('/webhook', (request, response) => {
-    //Create an instance
-    const _agent = new WebhookClient({request, response});
+// app.post('/webhook', (request, response) => {
+//     //Create an instance
+//     const _agent = new WebhookClient({request, response});
 
-    function welcome(agent) {
-        agent.add(`Welcome to my agent!`);
-    }
+//     function welcome(agent) {
+//         agent.add(`Welcome to my agent!`);
+//     }
 
-    function languageHandler(agent) {
-        const language = agent.parameters.language;
-        const programmingLanguage = agent.parameters['language-programming'];
-        if (language) {
-            agent.add(`From fulfillment: Wow! I didn't know you knew ${language}`);
-        } else if (programmingLanguage) {
-            agent.add(`From fulfillment: ${programmingLanguage} is cool`);
-        } else {
-            agent.add(`From fulfillment: What language do you know?`);
-        }
-    }
+//     function languageHandler(agent) {
+//         const language = agent.parameters.language;
+//         const programmingLanguage = agent.parameters['language-programming'];
+//         if (language) {
+//             agent.add(`From fulfillment: Wow! I didn't know you knew ${language}`);
+//         } else if (programmingLanguage) {
+//             agent.add(`From fulfillment: ${programmingLanguage} is cool`);
+//         } else {
+//             agent.add(`From fulfillment: What language do you know?`);
+//         }
+//     }
 
-    let intents = new Map();
+//     let intents = new Map();
 
-    intents.set('Default Welcome Intent', languageHandler);
+//     intents.set('Default Welcome Intent', languageHandler);
 
-    _agent.handleRequest(intents);
-})
+//     _agent.handleRequest(intents);
+// });
 
 initWebRoute(app);
 
