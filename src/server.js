263820a5db989b655cb34809b28ba61e96ab6d88
= <=>
2021-10-27 01:48:59 +0800

    update

--- src/server.js
+++ src/server.js
@@ -5,6 +5,10 @@ const bodyParser = require('body-parser');
 
 require('dotenv').config();
 let app = express();
+
+const { WebhookClient } = require('dialogflow-fulfillment');
+
+
  
 viewEngine(app);
 
@@ -13,6 +17,20 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 initWebRoute(app);
 
+
+app.post('/webhook', (req, res) => {
+     //Create an instance
+    const agent = new WebhookClient({request: request, response: response});
+
+    const Welcome = () => {
+        agent.add("Welcome to my agent!");
+    };
+
+    let intents = new Map();
+    agent.handleRequest(intents);
+
+    intents.set("Default welcome intent ", Welcome);    
+});
 
 
 let port = process.env.PORT || 8080;
